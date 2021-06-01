eda919a2e1dcf513f9b5c6f7ac8ffc2f65b102bb
Daniel Desmond <df.dezmond9@gmail.com>
2021-05-31 22:28:41 -0400

    cleaned this code up a bit created functions

--- scroll-program.py
+++ scroll-program.py
@@ -54,84 +54,90 @@ def binarySearch(arr, target):
 #         idx = word
 # print(minLen, idx)
 
-counter = 0
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 aphWNums = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-statement = ""
-
-current = alphabet
-
 print("Started at approximatley " + str(datetime.datetime.now()))
 
 #makes the statement that it searches for words in 
-while counter < 1000:
-   
-    rand = random.randint(0,len(current) - 1)
-    letter = current[rand]
-    statement += letter
-    # time.sleep(0.01)
-    # print(statement)
-    counter += 1
 
-print(statement)
+def createStatementOfficial(length, source):
+    final = ""
+    for _ in range(length):
+        rand = random.randint(0,len(source) - 1)
+        letter = source[rand]
+        final += letter
+        # time.sleep(0.01)
+        # print(final)
+    return final
+
+def createStatementFun(length, source):
+    final = ""
+    for _ in range(length):
+        rand = random.randint(0, len(source) -1)
+        letter = source[rand]
+        final += letter
+        time.sleep(0.01)
+        print(final)
 
 print("Finished selection at " + str(datetime.datetime.now()))
-binaryCounter = 0
-runningCounter = 0
-firstIdx = 0
-secondIdx = 0
-foundTotalUnique = 0
-foundTotal = 0
-impDict = orgAlph
-foundWords = []
-altRunner = 0
-given = statement
 
-#what finds the words
-while altRunner < len(given):
-    #calls binary search on the given letter
-    binaryResults = binarySearch(impDict, given[binaryCounter])
-    for word in impDict[binaryResults]:
-        if given[firstIdx:len(word) + secondIdx] == word:
-            if not word in foundWords:
-                #sifts through and decided if it is a new word or and old word and counts them as such
-                foundWords.append(word)
-                foundTotalUnique += 1
-                foundTotal += 1
-            else:
-                foundTotal += 1
-        runningCounter += 1
-        #check if we have looked at all possible words and and ups the indexes and zeros out the counter
-        if runningCounter == len(impDict[binaryResults]):    
-            firstIdx += 1
-            secondIdx += 1
-            runningCounter = 0
-        #gives us the percentages that it is at (time consuming unfortunately)
-        if altRunner == len(given) * 0.02 and word == impDict[binaryResults][0]:
-            print("2% of the way there! ========================")
-            print("At " +str(datetime.datetime.now()))
-        if altRunner == len(given) * 0.05 and word == impDict[binaryResults][0]:
-            print("5% of the way there! =================================")
-            print("At " +str(datetime.datetime.now()))
-        if altRunner == len(given) / 4 and word == impDict[binaryResults][0]:
-            print("25% of the way there! ==========================================")
-            print("At " + str(datetime.datetime.now()))
-        if altRunner == len(given) / 2 and word == impDict[binaryResults][0]:
-            print("50% of the way there! ===========================================================")
-            print("At " + str(datetime.datetime.now()))
-        if altRunner == len(given) * 0.75 and word == impDict[binaryResults][0]:
-            print("75% of the way there! ==================================================================================")
-            print("At " + str(datetime.datetime.now()))
-    #altRunner ensures that we don't create and infinite loop while also looking at ever possible letter
-    altRunner += 1
-    #ticks up the letter that is searched for in the binary search after each letter is fully looked at
-    binaryCounter += 1
+def searchStatement(given, reference):
+    binaryCounter = 0
+    runningCounter = 0
+    firstIdx = 0
+    secondIdx = 0
+    foundTotalUnique = 0
+    foundTotal = 0
+    foundWords = []
+    altRunner = 0
+    #what finds the words
+    while altRunner < len(given):
+        #calls binary search on the given letter
+        binaryResults = binarySearch(reference, given[binaryCounter])
+        for word in reference[binaryResults]:
+            if given[firstIdx:len(word) + secondIdx] == word:
+                if not word in foundWords:
+                    #sifts through and decided if it is a new word or and old word and counts them as such
+                    foundWords.append(word)
+                    foundTotalUnique += 1
+                    foundTotal += 1
+                else:
+                    foundTotal += 1
+            runningCounter += 1
+            #check if we have looked at all possible words and and ups the indexes and zeros out the counter
+            if runningCounter == len(reference[binaryResults]):    
+                firstIdx += 1
+                secondIdx += 1
+                runningCounter = 0
+            #gives us the percentages that it is at (time consuming unfortunately)
+            # if altRunner == len(given) * 0.02 and word == reference[binaryResults][0]:
+            #     print("2% of the way there! ========================")
+            #     print("At " +str(datetime.datetime.now()))
+            # if altRunner == len(given) * 0.05 and word == reference[binaryResults][0]:
+            #     print("5% of the way there! =================================")
+            #     print("At " +str(datetime.datetime.now()))
+            # if altRunner == len(given) / 4 and word == reference[binaryResults][0]:
+            #     print("25% of the way there! ==========================================")
+            #     print("At " + str(datetime.datetime.now()))
+            # if altRunner == len(given) / 2 and word == reference[binaryResults][0]:
+            #     print("50% of the way there! ===========================================================")
+            #     print("At " + str(datetime.datetime.now()))
+            # if altRunner == len(given) * 0.75 and word == reference[binaryResults][0]:
+            #     print("75% of the way there! ==================================================================================")
+            #     print("At " + str(datetime.datetime.now()))
+        #altRunner ensures that we don't create and infinite loop while also looking at ever possible letter
+        altRunner += 1
+        #ticks up the letter that is searched for in the binary search after each letter is fully looked at
+        binaryCounter += 1
+    return foundTotal, foundTotalUnique, foundWords
 
-print("The total amount of found words is: " +str(foundTotal))
-print("The total amount of unique words is: " +str(foundTotalUnique))
-print("I was able to find " +str(foundWords)) 
+totalFound, totalUniqueFound, wordsFound = searchStatement(createStatementOfficial(1000, alphabet), orgAlph)
+
+print("The total amount of found words is: " +str(totalFound))
+print("The total amount of unique words is: " +str(totalUniqueFound))
+print("I was able to find " +str(wordsFound)) 
 print("Finished up at " + str(datetime.datetime.now()))
 
+# createStatementFun(1000, alphabet)
