6425918207e194982cceced1752b6e0ae83a884e
Daniel Desmond <df.dezmond9@gmail.com>
2021-06-02 08:05:05 -0600

    statement can now be anaylized and graphed

--- scroll-program.py
+++ scroll-program.py
@@ -6,9 +6,13 @@ import time
 import datetime
 #list of words
 from english_words import english_words_lower_alpha_set
-
-from tqdm import *
-
+#imports a progress bar
+from tqdm import tqdm
+#need this to plot graphs
+from matplotlib import pyplot as plt 
+#histograms expect a np array
+import numpy as np
+#WORDS
 wordList = english_words_lower_alpha_set
 #test
 newWordList = []
@@ -60,7 +64,17 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 aphWNums = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-
+def statementAnalytics(statement):
+    sortedStatement = [[] for _ in range(26)]
+    percentages = []
+    for letter in statement:
+        sortedStatement[ord(letter) - 97].append(letter)
+    # print(sortedStatement)
+    for arr in sortedStatement:
+        percentages.append((arr[0], len(arr)/len(statement)))
+    #e,t,a,o,i
+    frequencyAnalysis = percentages[0][1] + percentages[4][1] + percentages[19][1] + percentages[14][1] + percentages[8][1]
+    return percentages, frequencyAnalysis
 
 #makes the statement that it searches for words in 
 
@@ -127,13 +141,52 @@ def searchStatement(given, reference):
     foundWords = list(set(foundWords))
     foundTotalUnique = len(foundWords)
 
-    return foundTotal, foundTotalUnique, foundWords
+    stateAnalytics, frequency = statementAnalytics(given)
 
-totalFound, totalUniqueFound, wordsFound = searchStatement(createStatementOfficial(1000000, alphabet), orgAlph)
+    # print(f"The total amount of found words is: {foundTotal}")
+    # print(f"The total amount of unique words is: {foundTotalUnique}")
+    # print(f"I was able to find {foundWords}") 
+    print(f"Statement analytics: \n {stateAnalytics}")
+    print(f"\nETAOI frequency is {frequency}")
+    print(f"Finished up at {datetime.datetime.now()}")
 
-print(f"The total amount of found words is: {totalFound}")
-print(f"The total amount of unique words is: {totalUniqueFound}")
-# print(f"I was able to find {wordsFound}") 
-print(f"Finished up at {datetime.datetime.now()}")
+    return foundTotal, foundTotalUnique, foundWords, stateAnalytics, frequency
+
+# totalFound, totalUniqueFound, wordsFound, analytics, frqAnalytics = searchStatement(createStatementOfficial(1000, alphabet), orgAlph)
+
+
+
+# etaoiTemp = [[] for _ in range(12)]
+its = [[] for _ in range(12)]
+for j in range(12):
+    newStatement = createStatementOfficial(10000, alphabet)
+    for i in range(50):
+        start = time.time()
+        print(f"Run {j+1}.{i+1}")
+        searchStatement(newStatement, orgAlph)
+        print("\n")
+        end = time.time()
+        # etaoiTemp = freq
+        its[j].append(10000 / (end - start))
+
+# etaoi = [etaoiTemp for _ in range(10)]
+
+print(its)
+
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9), (ax10, ax11, ax12)) = plt.subplots(4,3)
+
+ax1.hist(np.array(its[0]), range=(2000, 3500), bins=15)
+ax2.hist(np.array(its[1]), range=(2000, 3500), bins=15)
+ax3.hist(np.array(its[2]), range=(2000, 3500), bins=15)
+ax4.hist(np.array(its[3]), range=(2000, 3500), bins=15)
+ax5.hist(np.array(its[4]), range=(2000, 3500), bins=15)
+ax6.hist(np.array(its[5]), range=(2000, 3500), bins=15)
+ax7.hist(np.array(its[6]), range=(2000, 3500), bins=15)
+ax8.hist(np.array(its[7]), range=(2000, 3500), bins=15)
+ax9.hist(np.array(its[8]), range=(2000, 3500), bins=15)
+ax10.hist(np.array(its[9]), range=(2000, 3500), bins=15)
+ax11.hist(np.array(its[10]), range=(2000, 3500), bins=15)
+ax12.hist(np.array(its[11]), range=(2000, 3500), bins=15)
+plt.show()
 
 # createStatementFun(1000, alphabet)
