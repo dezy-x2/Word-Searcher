969ae58a0bdee78c9860c5850618986b99b9c0cd
Daniel Desmond <df.dezmond9@gmail.com>
2021-06-06 10:27:47 -1000

    added function to analyze and graph the it/s

--- scroll-program.py
+++ scroll-program.py
@@ -65,13 +65,13 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 aphWNums = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 def statementAnalytics(statement):
-    sortedStatement = [[] for _ in range(26)]
+    sortedStatement = [[None] for _ in range(26)]
     percentages = []
     for letter in statement:
         sortedStatement[ord(letter) - 97].append(letter)
     # print(sortedStatement)
     for arr in sortedStatement:
-        percentages.append((arr[0], len(arr)/len(statement)))
+        percentages.append((arr[-1], len(arr)/len(statement)))
     #e,t,a,o,i
     frequencyAnalysis = percentages[0][1] + percentages[4][1] + percentages[19][1] + percentages[14][1] + percentages[8][1]
     return percentages, frequencyAnalysis
@@ -140,53 +140,91 @@ def searchStatement(given, reference):
     foundTotal = len(foundWords)
     foundWords = list(set(foundWords))
     foundTotalUnique = len(foundWords)
+    stateAnalytics = None
+    frequency = None
 
     stateAnalytics, frequency = statementAnalytics(given)
 
-    # print(f"The total amount of found words is: {foundTotal}")
-    # print(f"The total amount of unique words is: {foundTotalUnique}")
-    # print(f"I was able to find {foundWords}") 
+    print(f"The total amount of found words is: {foundTotal}")
+    print(f"The total amount of unique words is: {foundTotalUnique}")
+    print(f"I was able to find {foundWords}") 
     print(f"Statement analytics: \n {stateAnalytics}")
     print(f"\nETAOI frequency is {frequency}")
     print(f"Finished up at {datetime.datetime.now()}")
 
     return foundTotal, foundTotalUnique, foundWords, stateAnalytics, frequency
 
-# totalFound, totalUniqueFound, wordsFound, analytics, frqAnalytics = searchStatement(createStatementOfficial(1000, alphabet), orgAlph)
+totalFound, totalUniqueFound, wordsFound, analytics, frqAnalytics = searchStatement(createStatementOfficial(1000, alphabet), orgAlph)
 
 
+def itpsAnalysis():
+    # etaoiTemp = [[] for _ in range(12)]
+    its = [[] for _ in range(12)]
+    for j in range(12):
+        newStatement = createStatementOfficial(200, alphabet)
+        for i in range(50):
+            start = time.time()
+            print(f"Run {j+1}.{i+1}")
+            searchStatement(newStatement, orgAlph)
+            print("\n")
+            end = time.time()
+            # etaoiTemp = freq
+            its[j].append(200 / (end - start))
 
-# etaoiTemp = [[] for _ in range(12)]
-its = [[] for _ in range(12)]
-for j in range(12):
-    newStatement = createStatementOfficial(10000, alphabet)
-    for i in range(50):
-        start = time.time()
-        print(f"Run {j+1}.{i+1}")
-        searchStatement(newStatement, orgAlph)
-        print("\n")
-        end = time.time()
-        # etaoiTemp = freq
-        its[j].append(10000 / (end - start))
+    print(its)
 
-# etaoi = [etaoiTemp for _ in range(10)]
+    fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9), (ax10, ax11, ax12)) = plt.subplots(4,3)
 
-print(its)
+    ax1.hist(np.array(its[0]), range=(2000, 3500), bins=15, histtype="step", color="red")
+    ax2.hist(np.array(its[1]), range=(2000, 3500), bins=15, histtype="step", color="blue")
+    ax3.hist(np.array(its[2]), range=(2000, 3500), bins=15, histtype="step", color="green")
+    ax4.hist(np.array(its[3]), range=(2000, 3500), bins=15, histtype="step", color="purple")
+    ax5.hist(np.array(its[4]), range=(2000, 3500), bins=15, histtype="step", color="orange")
+    ax6.hist(np.array(its[5]), range=(2000, 3500), bins=15, histtype="step", color="brown")
+    ax7.hist(np.array(its[6]), range=(2000, 3500), bins=15, histtype="step", color="black")
+    ax8.hist(np.array(its[7]), range=(2000, 3500), bins=15, histtype="step", color="pink")
+    ax9.hist(np.array(its[8]), range=(2000, 3500), bins=15, histtype="step", color="yellow")
+    ax10.hist(np.array(its[9]), range=(2000, 3500), bins=15, histtype="step", color="teal")
+    ax11.hist(np.array(its[10]), range=(2000, 3500), bins=15, histtype="step", color="aqua")
+    ax12.hist(np.array(its[11]), range=(2000, 3500), bins=15, histtype="step", color="violet")
+    plt.show()
 
-fig, ((ax1, ax2, ax3), (ax4, ax5, ax6), (ax7, ax8, ax9), (ax10, ax11, ax12)) = plt.subplots(4,3)
+def partialAlphabet():
+    uniqDiscovered = [[] for  _ in range(5)]
+    statementLen = 10000
+    for i in range(5):
+        for j in range(50):
+            newStatement = None
+            if i == 4:
+                newStatement = createStatementOfficial(statementLen, alphabet[i * 5:])
+            else:
+                newStatement = createStatementOfficial(statementLen, alphabet[i * 5:i * 5 + 5])
+            print(f"Run {i+1}.{j+1}")
+            _,total,_,_,_ = searchStatement(newStatement, orgAlph)
+            uniqDiscovered[i].append(total)
 
-ax1.hist(np.array(its[0]), range=(2000, 3500), bins=15)
-ax2.hist(np.array(its[1]), range=(2000, 3500), bins=15)
-ax3.hist(np.array(its[2]), range=(2000, 3500), bins=15)
-ax4.hist(np.array(its[3]), range=(2000, 3500), bins=15)
-ax5.hist(np.array(its[4]), range=(2000, 3500), bins=15)
-ax6.hist(np.array(its[5]), range=(2000, 3500), bins=15)
-ax7.hist(np.array(its[6]), range=(2000, 3500), bins=15)
-ax8.hist(np.array(its[7]), range=(2000, 3500), bins=15)
-ax9.hist(np.array(its[8]), range=(2000, 3500), bins=15)
-ax10.hist(np.array(its[9]), range=(2000, 3500), bins=15)
-ax11.hist(np.array(its[10]), range=(2000, 3500), bins=15)
-ax12.hist(np.array(its[11]), range=(2000, 3500), bins=15)
-plt.show()
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3,2)
+    ax1.hist(np.array(uniqDiscovered[0]), bins=50, range=(0,50))
+    ax2.hist(np.array(uniqDiscovered[1]), bins=50, range=(0,50))
+    ax3.hist(np.array(uniqDiscovered[2]), bins=50, range=(0,50))
+    ax4.hist(np.array(uniqDiscovered[3]), bins=50, range=(0,50))
+    ax5.hist(np.array(uniqDiscovered[4]), bins=50, range=(0,50))
+    ax6.bar([random.randint(1,5) for _ in range(5)], [random.randint(6,10) for _ in range(5)])
+    fig.suptitle(f"Partial Alphabet: {statementLen} chars")
+    plt.show()
+
+def justAToE():
+    discovered = []
+    statementLen = 10000
+    for i in range(500):
+        newStatement = createStatementOfficial(statementLen, alphabet[0:5])
+        print(f"Run {i+1}")
+        _,total,_,_,_ = searchStatement(newStatement, orgAlph)
+        discovered.append(total)
+    
+    plt.hist(np.array(discovered), bins=500, range=(0,50))
+    plt.show()
+
+# justAToE()
 
 # createStatementFun(1000, alphabet)
